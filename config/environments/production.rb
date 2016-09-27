45abafb187ba529d84dcc6f95f97e815dd35f41d
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2016-09-27 23:30:42 +0900

    Use SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,3 +23,13 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port    => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com'
+}
+ActionMailer::Base.delivery_method = :smtp
